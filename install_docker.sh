2fc12ea741f7d5199c0c0178a723fc5b02f02abf
Teddy Katayama <katayama@udel.edu>
2023-06-18 00:24:19 -0400

    Add files via upload

---
+++ install_docker.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install docker-ce
